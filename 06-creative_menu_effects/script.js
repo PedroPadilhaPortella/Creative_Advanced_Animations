276a865d9a99d7d8e1a3579d00fad40ce71c0520
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2025-06-29 23:23:35 -0300

    creative menu effects using css transform

---
+++ 06-creative_menu_effects/script.js
@@ -0,0 +1,31 @@
+const projects = [
+  "01_growing_borders_menu_effect",
+];
+
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+const projectsList = document.querySelector('.project-list');
+
+renderProjects();
+
+function renderProjects() {
+
+  projects.forEach((project) => {
+    const projectData = project.split('_');
+    const id = projectData.shift();
+
+    const card = document.createElement('div');
+    card.classList.add('project-card');
+
+    card.innerHTML = `
+      <a href="06-creative_menu_effects/${project}.html">
+        <p>${id} - ${projectData.map(capitalize).join(' ')}</p>
+        <p><span>View Projects</span> <i class="fas fa-arrow-right"></i></p>
+      </a>
+    `;
+
+    projectsList.append(card);
+  });
+}
