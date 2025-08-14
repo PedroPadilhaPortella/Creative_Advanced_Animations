d7c8347334b296fdf7c5fea8c9cb63fa7c8477a3
Pedro Henrique Padilha Portella da Cruz <pedro.kadjin.sg@gmail.com>
2025-08-13 23:36:44 -0300

    creative animations part 1

---
+++ 10-creative_animations/script.js
@@ -0,0 +1,36 @@
+const projects = [
+  "01_rotating_loading_animation",
+  "02_driving_animation",
+  "03_text_rotator_animation",
+  "04_animated_image_pattern",
+  "05_shaking_button_hover_effect",
+  "06_animated_button_hover_effect",
+];
+
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+const projectsList = document.querySelector('.project-list');
+
+renderProjects();
+
+function renderProjects() {
+
+  projects.forEach((project) => {
+    const projectData = project.split('_');
+    const id = projectData.shift();
+
+    const card = document.createElement('div');
+    card.classList.add('project-card');
+
+    card.innerHTML = `
+      <a href="10-creative_animations/${project}.html">
+        <p>${id} - ${projectData.map(capitalize).join(' ')}</p>
+        <p><span>View Projects</span> <i class="fas fa-arrow-right"></i></p>
+      </a>
+    `;
+
+    projectsList.append(card);
+  });
+}
