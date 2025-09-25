23288368b9c12ab16806563a1bacd15b3db4855c
Pedro Henrique Padilha Portella da Cruz <pedro.kadjin.sg@gmail.com>
2025-09-25 17:47:51 -0300

    creative animations part 5

--- 10-creative_animations/script.js
+++ 10-creative_animations/script.js
@@ -39,6 +39,11 @@ const projects = [
   "38_glowing_loader_animation",
   "39_cloud_rain_drops_animation",
   "40_text_fading_effect",
+  "41_neon_text_reveal_effect",
+  "42_dots_loading_animation",
+  "43_neon_button_effect",
+  "44_glassmorphism_animation_effect",
+  "45_moon_animation",
 ];
 
 const capitalize = (word) => {
